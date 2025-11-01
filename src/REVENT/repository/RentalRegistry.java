05f71c3cf0cf4bf78864b3b5c514b867f68dddad
Kristina Ahlenius <kristinaahlenius@gmail.com>
2025-11-01 22:14:49 +0100

    Rentallist to private

--- src/REVENT/repository/RentalRegistry.java
+++ src/REVENT/repository/RentalRegistry.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
 
 
 public class RentalRegistry {
-    protected HashMap<Member,Rental> rentalList = new HashMap<Member,Rental>();// borde ändras till <Member,List<Rental> .med Rentalobjekts lista som värde så kan samma medlem hyra flera saker.
+    private HashMap<Member,Rental> rentalList = new HashMap<Member,Rental>();// borde ändras till <Member,List<Rental> .med Rentalobjekts lista som värde så kan samma medlem hyra flera saker.
 
 public RentalRegistry(){}
 
